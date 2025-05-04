69736b733b3a305e79ffad8df0ac03ecd4555024
Sidda663 <n.siddu663@gmail.com>
2025-05-04 19:30:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Portfolio loaded');
 });
+
+const toggleBtn = document.getElementById('toggle-btn');
+const navLinks = document.getElementById('nav-links');
+
+toggleBtn.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
